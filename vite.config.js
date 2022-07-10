fc81b206049dcf89b1d136a3658eaa105fb0c0aa
Mirza <mirza.arnaut45@gmail.com>
2022-07-10 19:03:00 +0200

    Build svelte with a base url for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const BASE_URL = process.env.BASE_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: `${BASE_URL}/`
 })
