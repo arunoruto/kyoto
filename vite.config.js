1c10f4130f5c6ea08167ca9a17ed28e55b66bcd8
Mirza Arnaut <mirza.arnaut45@gmail.com>
2022-08-14 19:37:33 +0200

    Add console log and add front slash

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import autoPreprocess from 'svelte-preprocess';
 
 const BASE_URL = process.env.BASE_URL || '';
+console.log(BASE_URL);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,6 @@ export default defineConfig({
   plugins: [svelte({
     preprocess: autoPreprocess()
   })],
-  base: BASE_URL + '/'
-  //base: `${BASE_URL}/`
+  //base: BASE_URL + '/'
+  base: `/${BASE_URL}/`
 })
