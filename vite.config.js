acd6735e31eb86f1755cd46c980703963bd740c6
Mirza Arnaut <mirza.arnaut45@gmail.com>
2022-08-14 20:07:11 +0200

    Use ternaty operator

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import autoPreprocess from 'svelte-preprocess';
 
-const BASE_URL = `/${process.env.BASE_URL}/` || '';
+const BASE_URL = process.env.BASE_URL ? `/${process.env.BASE_URL}/` : '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
